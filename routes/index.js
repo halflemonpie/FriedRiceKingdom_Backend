4f57e8a65892126498b813e7fc8cfd14a67f3e2a
Mohammed <moejaffery@gmail.com>
2022-06-24 23:03:22 -0400

    added router for tasks

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/projects', require('./project'));
+router.use('/tasks', require('./task'));
 
 module.exports = router;
