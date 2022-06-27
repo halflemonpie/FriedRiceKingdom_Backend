c4ef60135ca28bead3225e2cb079aa98f948438d
Kevin <kabreu11385@gmail.COM>
2022-06-27 12:11:37 -0400

    fixed error

--- models/Project.js
+++ models/Project.js
@@ -2,13 +2,13 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-<<<<<<< HEAD
   name: String,
   description: String,
   category: String,
   importance: Number,
   date: Date,
   complete: Boolean,
+  image: String,
   tasks: [
     {
       ref: 'Task',
@@ -18,22 +18,3 @@ const projectSchema = new Schema({
 });
 const Project = mongoose.model('Project', projectSchema);
 module.exports = Project;
-=======
-    name: String,
-    description: String,
-    category: String,
-    importance: Number,
-    type: String,
-    date: Date,
-    complete: Boolean,
-    image: String,
-    tasks:[
-        {
-        ref: "Task",
-        type: mongoose.Schema.Types.ObjectId
-        }
-       ]
-  })
-  const Project = mongoose.model('Project', projectSchema)
-  module.exports = Project
->>>>>>> 102ccc2224da479617967f5ef7ec4530187a0b3f
