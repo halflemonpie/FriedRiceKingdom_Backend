4c00108fa893e585a750e0f73d61d4312d787d0d
Kevin <kabreu11385@gmail.COM>
2022-06-27 12:04:14 -0400

    deleted type key

--- models/Project.js
+++ models/Project.js
@@ -1,20 +1,19 @@
-const mongoose = require('../db/connection')
-const Schema = mongoose.Schema
+const mongoose = require('../db/connection');
+const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-    name: String,
-    description: String,
-    category: String,
-    importance: Number,
-    type: String,
-    date: Date,
-    complete: Boolean,
-    tasks:[
-        {
-        ref: "Task",
-        type: mongoose.Schema.Types.ObjectId
-        }
-       ]
-  })
-  const Project = mongoose.model('Project', projectSchema)
-  module.exports = Project
+  name: String,
+  description: String,
+  category: String,
+  importance: Number,
+  date: Date,
+  complete: Boolean,
+  tasks: [
+    {
+      ref: 'Task',
+      type: mongoose.Schema.Types.ObjectId,
+    },
+  ],
+});
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
