877192c5225fa2ff777f2d92590b156b4040d1b3
Yonghai <dylanyonghaih@gmail.com>
2022-06-28 23:07:45 -0400

    testing

--- models/project.js
+++ models/project.js
@@ -2,19 +2,17 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-  name: String,
-  description: String,
-  category: String,
-  importance: Number,
-  date: Date,
-  complete: Boolean,
-  image: String,
-  tasks: [
-    {
-      ref: 'Task',
-      type: Object,
-    },
-  ],
-});
-const Project = mongoose.model('Project', projectSchema);
-module.exports = Project;
+    name: String,
+    description: String,
+    category: String,
+    importance: Number,
+    date: Date,
+    complete: Boolean,
+    image: String,
+    tasks:[{
+        ref: "Task",
+        type: Object
+    }]
+  })
+  const Project = mongoose.model('Project', projectSchema)
+  module.exports = Project
