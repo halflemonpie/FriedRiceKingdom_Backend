ce1cc228c6db71ae173830e97e090789062782a5
mjaffery1 <99685970+mjaffery1@users.noreply.github.com>
2022-06-28 21:57:23 -0400

    Delete task.js

--- models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../db/connection')
-const Schema = mongoose.Schema
-
-const taskSchema = new Schema({
-    name: String,
-    complete: Boolean,
-  })
-  const Task = mongoose.model('Task', taskSchema)
-  module.exports = Task
